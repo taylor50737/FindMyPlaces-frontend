b70327c7c8a95d4a3dce9eb9d676fa3764ce9a1a
Taylor Lo <taylorloph@gmail.com>
2023-06-27 16:26:13 -0400

    Setup SignUp fetch API

--- src/places/pages/UserPlaces.js
+++ src/places/pages/UserPlaces.js
@@ -6,6 +6,20 @@ import PlaceList from "../components/PlaceList";
 const DUMMY_PLACES = [
   {
     id: "p1",
+    title: "Seneca Polytechnic College Newnham Campus",
+    description:
+      "Seneca College of Applied Arts and Technology, operating as Seneca Polytechnic. is a multiple-campus public college in the Greater Toronto Area, and Peterborough, Ontario, Canada regions. It offers full-time and part-time programs at the baccalaureate, diploma, certificate, and graduate levels.",
+    imageUrl:
+      "https://www.vmcdn.ca/f/files/newmarkettoday/images/education/2020-09-26-seneca-college.jpg;w=960",
+    address: "1750 Finch Ave E, North York, ON M2J 2X5",
+    location: {
+      lat: 43.796130045115646,
+      lng: -79.34852742957067,
+    },
+    creator: "u1",
+  },
+  {
+    id: "p2",
     title: "Empire State Building",
     description: "One of the most famous sky scrapers in the world!",
     imageUrl:
@@ -19,7 +33,7 @@ const DUMMY_PLACES = [
   },
   {
     id: "p2",
-    title: "Emp. State Building",
+    title: "Empire State Building",
     description: "One of the most famous sky scrapers in the world!",
     imageUrl:
       "https://upload.wikimedia.org/wikipedia/commons/thumb/1/10/Empire_State_Building_%28aerial_view%29.jpg/800px-Empire_State_Building_%28aerial_view%29.jpg",
--- src/user/pages/Users.js
+++ src/user/pages/Users.js
@@ -5,10 +5,17 @@ const Users = () => {
   const USERS = [
     {
       id: "u1",
-      name: "Taylor Lo",
+      name: "Taylor",
       image:
         "https://i.etsystatic.com/25924315/r/il/b87247/4826173692/il_794xN.4826173692_e7xp.jpg",
-      places: 3,
+      places: 2,
+    },
+    {
+      id: "u2",
+      name: "Kuri",
+      image:
+        "https://www.thesprucepets.com/thmb/GEbKdMXYZLs4jIrdx2Xm9K0EK74=/750x0/filters:no_upscale():max_bytes(150000):strip_icc():format(webp)/top-friendliest-dog-breeds-4691511_hero-5c6a918dcf56409c888d78b0fac82d18.jpg",
+      places: 1,
     },
   ];
 
