8f3b7512f4473b0d20d2b97cb8bfa98837be1150
Taylor Lo <taylorloph@gmail.com>
2023-07-19 23:20:56 -0400

    Added delete places DELETE request

--- src/places/components/PlaceList.js
+++ src/places/components/PlaceList.js
@@ -29,6 +29,7 @@ const PlaceList = (props) => {
           address={place.address}
           creatorId={place.creator}
           coordinates={place.location}
+          onDelete={props.onDeletePlace}
         />
       ))}
     </ul>
